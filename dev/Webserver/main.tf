2b7742ca23c559254dd4ee2549185f7496ba6e92
shailendrakushwaha03 <148499784+shailendrakushwaha03@users.noreply.github.com>
2025-02-01 15:03:55 -0500

    Update main.tf

--- dev/Webserver/main.tf
+++ dev/Webserver/main.tf
@@ -65,6 +65,14 @@ resource "aws_security_group" "security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    description = "Allow HTTP"
+    from_port   = 8081
+    to_port     = 8083
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
